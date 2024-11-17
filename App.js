0bfdcdf09dfb65c5c2f2bbc024d71b362a27483a
ARSH AMIRALI <arshamirali37>
2024-11-17 21:06:30 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+// TERNARY Operator
+// {ES6}
+
+// Case1:-
+// let age = +prompt("Enter your age")
+// let gender= 'male'
+// let check=age==25?'Allow':"Not Allow"
+// console.log(check);
+
+
+//Case2 :-
+// let age=+prompt('Enter your age')
+// let gender=prompt('Enter your gender')
+// let check= age===25?gender==="male"?'allow':"gender not match":"age not match"
+// console.log(check)
+
+
+// Task Percentage Checker
+let percentage=prompt("Enter your percentage in exam")
+let check =percentage>100?"percentage not correct":  percentage>90 ? "A+" : percentage>80? "A":percentage>70?"B":percentage>60?"C":percentage>50?"D":"Ungraded"
+console.log(check);
+
+
+
+
+
+/*
+2 dropdown
+
+1st mobile brand 
+2nd 
+
+*/
